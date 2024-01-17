19dc29e2c6d5acb363cf924fc1a59a85046c0cfe
ruddro42001~ <aliruddro@gmail.com>
2024-01-17 16:11:02 +0600

    minor changes done

--- src/components/Mentor.jsx
+++ src/components/Mentor.jsx
@@ -17,53 +17,6 @@ const Mentor = () => {
                             <Card className="max-w-lg mentor-block mt-3">
                                 <div className="flex flex-col items-center pb-10">
                                     <img
-                                        src="/img/mentor/mfahad.png"
-                                        alt="ruddro-mentor"
-                                        className="mb-3 rounded-full shadow-lg"
-                                        height="96"
-                                        width="96"
-                                    />
-                                    <h5 className="mb-1 text-xl font-medium text-[#12c2e9]">Mohiuddin Fahad</h5>
-                                    <h6 className="text-sm text-white">CEO & Founder, Inspired IT </h6>
-                                    <p className='text-gray-500 mt-5 text-justify'>সৈয়দ মুহীউদ্দীন ফাহাদ , গত  বছর ৮ বছর যাবত ফাইভার, আপওয়ার্ক সহ বিভিন্ন লোকাল মার্কেটপ্লেসে কাজ করছেন । এছাড়াও  ২০১৮ সালে তিনি প্রতিষ্ঠা করেন ইন্সপায়ার্ড আইটি যেটি বাংলাদেশের বেকারত্ব সমস্যা সমাধানে সুনির্দিষ্ট ও সুনির্ধারিত কোর্স মডিউল অনুসরন করে বিভিন্ন সেক্টরে মানুষকে দক্ষ করে তোলার পাশাপাশি তাদের কাজের সুযোগ তৈরী করে দিতে বিভিন্ন ভাবে কাজ করে যাচ্ছে। </p>
-                                    <div className="mt-4 flex space-x-3 lg:mt-6">
-                                        <a
-                                            href="https://www.facebook.com/mfahadbd668"
-                                            className="inline-flex items-center rounded-lg bg-[#0866FF] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                                            target='_blank' rel="noreferrer"
-                                        >
-                                            <FaFacebook />
-                                        </a>
-                                        <a
-                                            href="https://www.linkedin.com/in/mfahad668"
-                                            className="inline-flex items-center rounded-lg bg-[#0A66C2] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                                            target='_blank' rel="noreferrer"
-                                        >
-                                            <FaLinkedin />
-                                        </a>
-                                        <a
-                                            href="https://www.behance.net/mfahadbd"
-                                            className="inline-flex items-center rounded-lg bg-[#FF00FF] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                                            target='_blank' rel="noreferrer"
-                                        >
-                                            <FaBehance />
-                                        </a>
-                                        <a
-                                            href="https://mfahadbd.com/"
-                                            className="inline-flex items-center rounded-lg bg-[#EF546D] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                                            target='_blank' rel="noreferrer"
-                                        >
-                                            <TbWorldWww />
-                                        </a>
-
-                                    </div>
-                                </div>
-                            </Card>
-                        </div>
-                        <div className="mentor-one">
-                            <Card className="max-w-lg mentor-block mt-3">
-                                <div className="flex flex-col items-center pb-10">
-                                    <img
                                         src="/img/mentor/ruddro.png"
                                         alt="ruddro-mentor"
                                         className="mb-3 rounded-full shadow-lg"
@@ -97,6 +50,53 @@ const Mentor = () => {
                                         </a>
                                         <a
                                             href="https://ruddro.info/"
+                                            className="inline-flex items-center rounded-lg bg-[#EF546D] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                                            target='_blank' rel="noreferrer"
+                                        >
+                                            <TbWorldWww />
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </Card>
+                        </div>
+                        <div className="mentor-one">
+                            <Card className="max-w-lg mentor-block mt-3">
+                                <div className="flex flex-col items-center pb-10">
+                                    <img
+                                        src="/img/mentor/mfahad.png"
+                                        alt="ruddro-mentor"
+                                        className="mb-3 rounded-full shadow-lg"
+                                        height="96"
+                                        width="96"
+                                    />
+                                    <h5 className="mb-1 text-xl font-medium text-[#12c2e9]">Mohiuddin Fahad</h5>
+                                    <h6 className="text-sm text-white">CEO & Founder, Inspired IT </h6>
+                                    <p className='text-gray-500 mt-5 text-justify'>সৈয়দ মুহীউদ্দীন ফাহাদ , গত  বছর ৮ বছর যাবত ফাইভার, আপওয়ার্ক সহ বিভিন্ন লোকাল মার্কেটপ্লেসে কাজ করছেন । এছাড়াও  ২০১৮ সালে তিনি প্রতিষ্ঠা করেন ইন্সপায়ার্ড আইটি যেটি বাংলাদেশের বেকারত্ব সমস্যা সমাধানে সুনির্দিষ্ট ও সুনির্ধারিত কোর্স মডিউল অনুসরন করে বিভিন্ন সেক্টরে মানুষকে দক্ষ করে তোলার পাশাপাশি তাদের কাজের সুযোগ তৈরী করে দিতে বিভিন্ন ভাবে কাজ করে যাচ্ছে। </p>
+                                    <div className="mt-4 flex space-x-3 lg:mt-6">
+                                        <a
+                                            href="https://www.facebook.com/mfahadbd668"
+                                            className="inline-flex items-center rounded-lg bg-[#0866FF] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                                            target='_blank' rel="noreferrer"
+                                        >
+                                            <FaFacebook />
+                                        </a>
+                                        <a
+                                            href="https://www.linkedin.com/in/mfahad668"
+                                            className="inline-flex items-center rounded-lg bg-[#0A66C2] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                                            target='_blank' rel="noreferrer"
+                                        >
+                                            <FaLinkedin />
+                                        </a>
+                                        <a
+                                            href="https://www.behance.net/mfahadbd"
+                                            className="inline-flex items-center rounded-lg bg-[#FF00FF] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                                            target='_blank' rel="noreferrer"
+                                        >
+                                            <FaBehance />
+                                        </a>
+                                        <a
+                                            href="https://mfahadbd.com/"
                                             className="inline-flex items-center rounded-lg bg-[#EF546D] px-4 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
                                             target='_blank' rel="noreferrer"
                                         >
