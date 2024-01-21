f58716dd5be87f089f028d72fdaa9bd6539c02c2
ruddro42001~ <aliruddro@gmail.com>
2024-01-21 16:48:19 +0600

    code optimization

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,6 +1,16 @@
 import { Card } from "flowbite-react";
 
 const Language = () => {
+    const languages = [
+        { id: 1, name: 'HTML', imgSrc: '/img/hero/html.png' },
+        { id: 2, name: 'CSS', imgSrc: '/img/hero/css.png' },
+        { id: 3, name: 'SASS', imgSrc: '/img/hero/sass.png' },
+        { id: 4, name: 'BOOTSTRAP', imgSrc: '/img/hero/bootstrap.png' },
+        { id: 5, name: 'JAVASCRIPT', imgSrc: '/img/hero/js.png' },
+        { id: 6, name: 'REACT', imgSrc: '/img/hero/react.png' },
+        { id: 6, name: 'WORDPRESS', imgSrc: '/img/hero/wordpress.png' },
+        { id: 6, name: 'GITHUB', imgSrc: '/img/hero/github.png' },
+    ]
     return (
         <div>
             <section id="course-technology" className="bg-section-language">
@@ -13,62 +23,17 @@ const Language = () => {
                         <p className="text-gray-500 sm:text-xl">এই কোর্সের ল্যাংগুয়েজ</p>
                     </div>
                     <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-4 md:gap-8 md:space-y-0">
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/html.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    HTML
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/css.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    CSS
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/sass.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    SASS
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/bootstrap.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    BOOTSTRAP
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/js.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    JAVASCRIPT
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/react.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    REACT
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/wordpress.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                    WORDPRESS
-                                </h5>
-                            </Card>
-                        </div>
-                        <div className="language-block">
-                            <Card className="language-img" imgSrc="/img/hero/github.png" horizontal>
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
-                                GITHUB
-                                </h5>
-                            </Card>
-                        </div>
+                        {
+                            languages.map(language => (
+                                <div key={language.id} className="language-block">
+                                    <Card className="language-img" imgSrc={language.imgSrc} horizontal>
+                                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 mbl-heading">
+                                            {language.name}
+                                        </h5>
+                                    </Card>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </section>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,25 @@
 
 const Project = () => {
+    const project = [
+        { id: 1, src: '/img/projects/1.png' },
+        { id: 2, src: '/img/projects/2.png' },
+        { id: 3, src: '/img/projects/3.png' },
+        { id: 4, src: '/img/projects/4.png' },
+        { id: 5, src: '/img/projects/5.png' },
+        { id: 6, src: '/img/projects/6.png' },
+        { id: 7, src: '/img/projects/7.png' },
+        { id: 8, src: '/img/projects/8.png' },
+        { id: 9, src: '/img/projects/9.png' },
+        { id: 10, src: '/img/projects/10.png' },
+        { id: 11, src: '/img/projects/11.png' },
+        { id: 12, src: '/img/projects/12.png' },
+    ]
     return (
         <div>
             <section id="course-project" className="bg-section-project">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
                     <div className=" mb-10 lg:mb-16 text-center">
-                    <div className="header-img">
+                        <div className="header-img">
                             <img src="/img/section/project.svg" />
                         </div>
                         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">কোর্সে যে <span>প্রজেক্ট</span> গুলো করানো হবে</h2>
@@ -13,42 +27,13 @@ const Project = () => {
                     </div>
                     {/* Project Gallery show here */}
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-4 project-img">
-                        <div className="">
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/1.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/2.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/3.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/4.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/5.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/6.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/7.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/8.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/9.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/10.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/11.png" alt="" />
-                        </div>
-                        <div>
-                            <img className="h-auto max-w-full rounded-lg" src="/img/projects/12.png" alt="" />
-                        </div>
+                        {
+                            project.map(project => (
+                                <div key={project.id} className="">
+                                    <img className="h-auto max-w-full rounded-lg" src={project.src} alt="" />
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </section>
